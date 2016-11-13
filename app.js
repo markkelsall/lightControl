7995bb212c997f832664a0b033ecd95bad619819
Mark Kelsall <mark.kelsall@gmail.com>
2016-11-13 23:55:09 +0000

    add routes and projectAddress

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const server = require('../expressServer/server');
 let serverConfig = {
     port: 8080,
-    address: 'lightControl'
+    address: 'lightControl',
+    projectAddress: __dirname,
+    routes: [
+        {
+            name: 'home',
+            method: 'GET',
+            address: '/',
+            response: 'index.html'
+        }
+    ]
 };
 server.start(serverConfig);
